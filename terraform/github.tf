b2a2169b2589d4647711a8611ea7bec21ce8f637
Kevin Wang <kwangsan@gmail.com>
2022-08-27 11:28:22 -0400

    feat: import `nextjs-components`

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,3 +1,8 @@
+# Default GitHub provider
+# This may be overridden by individual resources using the
+# `provider` meta-argument
+# 
+# https://www.terraform.io/language/meta-arguments/resource-provider
 provider "github" {
   owner = "thiskevinwang"
   token = var.gh_token
