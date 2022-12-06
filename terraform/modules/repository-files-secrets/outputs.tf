bcc1121f8f9c89775163f79fc09d2415409f9a1b
Kevin Wang <kwangsan@gmail.com>
2022-12-06 00:42:26 -0500

    rm output

--- terraform/modules/repository-files-secrets/outputs.tf
+++ terraform/modules/repository-files-secrets/outputs.tf
@@ -1,3 +1,3 @@
-output "pull_request_url" {
-  value = format("%s/pull/%s", data.github_repository.repository.html_url, github_repository_pull_request.pull_request.number)
-}
+# output "pull_request_url" {
+#   value = format("%s/pull/%s", data.github_repository.repository.html_url, github_repository_pull_request.pull_request.number)
+# }
