d2bd75f639f1ec5e58a01e3e5e9e469595ba8309
Kevin Wang <kwangsan@gmail.com>
2022-12-06 00:45:09 -0500

    fix eventual output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,15 @@
 # Output each repository-settings' pull request URL.
-output "pull_request" {
+# output "pull_request" {
+#   value = {
+#     for k, v in module.repository-files-secrets :
+#     k => v.pull_request_url
+#   }
+# }
+
+
+output "branch" {
   value = {
     for k, v in module.repository-files-secrets :
-    k => v.pull_request_url
+    k => v.branch
   }
 }
-
